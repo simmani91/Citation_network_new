40da87b5170cfa9df2b3ba256f72560ed33d6771
simmani91 <devsimmani@gmail.com>
2016-04-21 11:55:42 +0900

    git으로 코드 관리하기, 데이터 용량이 너무 커서 일단은 코드들만 이쪽으로 관리할 수 있도록 한다.

---
+++ code.py
@@ -0,0 +1,10 @@
+#-*-encoding: utf-8 -*-
+# ipython notebook --ip 0.0.0.0 --port 9999
+#import networkx as nx
+import numpy as np
+import re
+import os.path
+import csv
+from scipy.stats import rankdata
+import scipy.stats
+import time
