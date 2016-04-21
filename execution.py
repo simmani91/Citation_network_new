e4e0b88abdc2113fd75dc7edb92e9ef04a7ccd93
simmani91 <devsimmani@gmail.com>
2016-04-21 13:34:40 +0900

    partial 데이터를 잘 parsing한code 임시 저장

---
+++ execution.py
@@ -0,0 +1,5 @@
+import code
+processed_data_input_file_path = "../1.raw_data/acm_partial.txt"
+processed_data_output_file_path = "../2.processed_data/"
+
+code.processed_data(processed_data_input_file_path,processed_data_output_file_path)
