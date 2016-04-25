3b082f6c786b7f472bf117f567206e47be7e2def
simmani91 <devsimmani@gmail.com>
2016-04-25 12:44:45 +0900

    연도가 역전되었는지 에러를 채크해보니 에러는 없엇다. 그러나  연도와 제목이 없는 아이디가 300만개정도 나왔다. 1000만개 중에 300만개가 리스트 안에 없는 데이터가 되는 셈, 예전에 이걸로 고생한 기억이 있으므로 cited_count_all을 다시 정재해야겠다.

--- _1_raw_to_processed_parser.py
+++ _1_raw_to_processed_parser.py
@@ -36,25 +36,7 @@ def save_processed_data(item,out_path):
 	global f_out_id_year_abstract	
 	global check
 
-	if item.year <1900:
-		if item.year in check :
-			check[item.year] = check[item.year] + 1
-		else:
-			check[item.year] = 1
-		"""
-		print item.year
-		print item.item_id
-		print item.title
-		
-		print item.publisher
-		print item.author
-		print item.cite
-		print item.abstract
-		"""
 
-	if item.year == 11:
-		print item.item_id
-		print item.title		
 	f_out_id_year_title.write(item.item_id.strip()+"|"+str(item.year)+"|"+item.title.strip()+"\n")
 	f_out_publisher.write(item.item_id.strip()+"|"+str(item.year)+"|"+item.publisher.strip()+"\n")
 	f_out_id_year_abstract.write(item.item_id.strip()+"|"+str(item.year)+"|"+item.abstract.strip() + "\n")
@@ -112,27 +94,178 @@ def processed_data(in_path, out_path):
 		if len(line) > 1:
 			flag = line[1]
 			if (flag == "*"):
-				item.title = line[2:].strip()
+				item.title = line[2:].strip().replace("\t"," ")
 			elif(flag == "@"):
 				item.author = line[2:].split(",")
 			elif(flag == "t"):
 				item.year = int(line[2:].strip())
 			elif(flag == "c"):
-				item.publisher = line[2:]
+				item.publisher = line[2:].strip().replace("\t"," ")
 			elif(flag == "i"):
 				item.item_id = line[6:].strip()
 			elif(flag == "%"):
 				item.cite.append(line[2:])
 			elif(flag == "!"):
-				item.abstract = line[2:]
+				item.abstract = line[2:].replace("\t"," ")
+				#임시로 막아놓음
 				save_processed_data(item,out_path)
-				save_customized_data(item,out_path)
+				#save_customized_data(item,out_path)
+
 				item = Item()
 	print "=============	Parsing raw data end 	=============="
 	print("Parsing raw data takes %s seconds" % (time.time() - temp_start_time))
-	for i in check:
-		print str(i) + ": "+ str(check[i])
 
+	"""
+	f_out = open("error_title.txt", "w")
+	for i in check:
+		f_out.write(str(i.replace("\t"," ")) + "\n")
+	"""
 	temp_start_time = time.time()
 
 
+def check_year_error():
+	#연도가 이상한 데이터를 추출한것을 하나하나 검사하여 쓸 수 있는데이터(연도 추측가능)는 연도를 추가하고 
+	#쓸수 없는 데이터는 no relation에 적어놓는다. 
+	data_in_id_year = "../2.processed_data/all_id_year_title.txt"
+	data_in_cite = "../2.processed_data/all_cite.txt"
+	all_id_year =  {}
+	all_cite = {}
+
+
+	f_in_id_year = open(data_in_id_year, "r")
+	f_in_cite = open(data_in_cite, "r")
+	f_in_error = open("error_title.txt", "r")
+
+	lines = f_in_id_year.readlines()
+	f_in_id_year.close()
+
+	for line in lines:
+		parse = line.split("|")
+		item_id = parse[0]
+		year = int(parse[1])
+
+		all_id_year[item_id] = year
+
+
+	lines = f_in_cite.readlines()
+	f_in_cite.close()
+
+	for line in lines:
+		parse = line.split(",")
+		item_id_A = parse[0].strip()
+		item_id_B = parse[1].strip()
+
+		if (item_id_A not in all_cite):
+			all_cite[item_id_A] = []	
+		all_cite[item_id_A].append(item_id_B)
+		
+	print "all_cite: " + str(len(all_cite))
+	print "all_id: " + str(len(all_id_year))
+	ans = {}
+	lines = f_in_error.readlines()
+	print "all_id: " + str(len(all_id_year))
+
+	f_out_no_relation = open("No_relation.txt","w")
+	for line in lines:
+		item_year = 0 
+		if (line.strip() not in all_cite):
+			print "No relations: " + str(line.strip())
+			f_out_no_relation.write(line.strip() + "\n")
+			continue
+
+		for item_id in all_cite[line.strip()]:
+			try:
+				cited_item_year = all_id_year[item_id]
+			except:
+				print line.strip() + " " + item_id
+			if item_year < cited_item_year:
+				item_year = cited_item_year
+		ans[line.strip()]= item_year + 1
+
+	print "ans: " + str(len(ans))
+	f_out = open("error_title_correct.txt", "w")
+	for item in ans:
+		if ans[item] > 1900:
+			f_out.write(str(item) + "," + str(ans[item])+ "\n")
+		else: 
+			f_out_no_relation.write(str(item.strip())+ "\n")
+
+def refining_data():
+	#연도 이상한거 acm.txt에서 삭제하는 도구
+	f_in_no_relation = open("No_relation.txt","r")
+	f_in_correct = open("error_title_correct.txt","r")
+	f_in_acm = open("../1.raw_data/acm.txt", "r")
+	f_out_acm = open("../1.raw_data/acm3.txt","w")
+
+	no_relation = {}
+	correct = {}
+
+	lines = f_in_no_relation.readlines()
+	for line in lines:
+		no_relation[line.strip()] = 1
+
+	lines = f_in_correct.readlines()
+	for line in lines:
+		parse = line.split(",")
+		correct[parse[0]] = int(parse[1])
+
+	flag = 0
+	lines = f_in_acm.readlines()
+	data = ["","","","","",[],""]
+	symbal = {'*':0,'@':1,'t':2,'c':3,'i':4,'%':5,'!':6 }
+	for line in lines:
+		if len(line)>1:
+			if (line[1] not in symbal):
+				continue
+			address = symbal[line[1]]
+			if (address == 0):
+				if (data[address] != ""):
+					if flag == 0:
+						#flag == 1이면 no relation이므로 출력하지 않을 것
+						for i in data:
+							if (type(i) == str):
+								if (i != ""):
+									f_out_acm.write(i)
+							elif(len(data[5]) >0):
+								for j in data[5]:
+									f_out_acm.write(j)
+						f_out_acm.write("\n")
+					data = ["","","","","",[],""]
+					flag = 0
+				data[address] = line
+			elif address == 2:
+				if (int(line[2:]) < 1900):
+					flag = 1
+				data[address] = line
+
+			elif (address == 5):
+				#다른 데이터와 달리 리스트를 사용하므로 예외처리
+				data[address].append(line)
+			elif(address ==4):
+				#아이템 아이디가 버리는건지, 쓸만한건지, 상관없는건지 확인하기 위해 예외처리 
+				item_id = line[6:].strip()
+				if (item_id in no_relation):
+					#flag =1로 설정, 출력하지 말라는 뜻 
+					flag = 1
+				elif (item_id in correct):
+					#조정된 연도가 있으면 그걸로 초기화 
+					data[2] = "#t" + str(correct[item_id])+"\n"
+					flag = 0
+				#상관없음 패스
+				data[address] = line
+			else:
+				#0,1,2,3의 경우 일반 제목, 저자, 퍼블리셔, 연도 이므로 그냥 저장 
+				data[address] = line
+
+			if (line == lines[len(lines)-1]):
+				#마지막 데이터 출력
+				for i in data:
+					if (type(i) == str):
+						if (i != ""):
+							f_out_acm.write(i)
+					elif(len(data[5]) >0):
+						for j in data[5]:
+							f_out_acm.write(j)
+				data = ["","","","","",[],""]
+				f_out_acm.write("\n")
+		
--- code.py
+++ code.py
@@ -1,6 +1,9 @@
 #-*-encoding: utf-8 -*-
 # ipython notebook --ip 0.0.0.0 --port 9999
+#import mathplotlib.
+#%matplotlib inline 이걸 써줘야 아이파이썬에서 가능
 #import networkx as nx
+
 import numpy as np
 import re
 import os.path
@@ -19,58 +22,147 @@ def templets():
 	temp_start_time = time.time()
 
 
-def temp():
-	data_in = "../2.processed_data/all_id_year_title.txt"
-	f_in = open(data_in, "r")
-	lines = f_in.readlines()
-
-	year_last = 0
-	year_first = 9999
-	for line in lines:
-		parse = line.split("|")
-		year = int(parse[1])
-		if (year > year_last):
-			year_last = year
-		if (year < year_first):
-			year_first = year
-
-	print year_last
-	print year_first
 
 #네트워크 관련 자료부터 시작한다. 
 
+def data_descriptor(in_path, out_path):
+
+	#입력받는 부분
+	f_in_1 = open(in_path + "all_cite.txt","r")
+	f_in_2 = open(in_path + "all_id_year_title.txt", "r")
+
+	f_out_source = open(out_path + "source_count.txt", "w")
+	f_out_target = open(out_path + "target_count.txt", "w")
+
+	id_year_title = f_in_2.readlines()
+	cite = f_in_1.readlines()
+	f_in_1.close()
+	f_in_2.close()
+
+	print "all item size:\t" + str(len(id_year_title))
+
+
+	#계산하는 부분 
+	item = {}
+	all_item = {}
+	for i in id_year_title:
+		parse = i.split("|")
+		item_id = parse[0]
+		year = int(parse[1])
+		if (year not in item):
+			item[item_id] = year
+
+	source_count = {}
+	target_count = {}
+
+	print "all relation size:\t" + str(len(cite))
+	for i in cite:
+		parse = i.split(",")
+		source = parse[0].strip()
+		target = parse[1].strip()
+		if (source not in source_count):
+			source_count[source] = 0
+		if (target not in target_count):
+			target_count[target] = 0
+
+		source_count[source] = source_count[source] + 1
+		target_count[target] = target_count[target] + 1
+
+	print "all_source:\t" + str(len(source_count))
+	print "all_target:\t" + str(len(target_count))
+
+	#출력하는 부분
+
+	for i in source_count:
+		f_out_source.write(str(source_count[i]) + "\n")
+		if i < 10000:
+			continue
+	for i in target_count:
+		if i < 10000:
+			continue
+		f_out_target.write(str(target_count[i]) + "\n")
+
+def plt_hist(x, title):
+	import numpy as np
+	import matplotlib
+	import matplotlib.pyplot as plt
+
+	matplotlib.use('Agg')
+
+	plt.hist(x, 5)
+
+	plt.xlabel('cited_count')
+	plt.ylabel('number_of_data')
+	plt.title('Hist_of_'+ title)
+	plt.axis([0, 200,10000,1050000])
+
+	plt.grid(True)
+	plt.show()
+
+def data_descriptor_2(in_path, out_path):
+	import matplotlib.pyplot as plt
+
+	#입력받는 부분 
+	f_in_source = open(in_path + "source_count.txt", "r")
+	f_in_target = open(in_path + "target_count.txt", "r")
+
+	source = map(int, f_in_source.readlines())
+	target = map(int, f_in_target.readlines())
+	target.remove(938038)
+
+	print "max source:\t" +str(max(source))
+	print "ave source:\t" +str(float(sum(source))/float(len(source)))
+	print "size source:\t" +str(len(source))
+	print "max target:\t" +str(max(target))
+	print "ave target:\t" +str(float(sum(target))/float(len(source)))
+	print "size target:\t" +str(len(source))
+
+
+	plt_hist(source, "Source")
+
+
+
+
 def cite_year_divider(in_path, out_path):
 	#연도별로 인용 링크를 나누어준다.
-	f_in_1 = open(in_path + "all_cite.txt","r")
-	f_in_2 = open( in_path + "all_id_year.txt", "r")
+	#만약 에러가 발생하면(인용하고있는 연도가 시기적으로 맞지 않는 경우) 이를 에러로 출력한다. 
 
-	t_year = f_in_2.readlines()
-	t_cite = f_in_1.readlines()
+	f_in_1 = open(in_path + "all_cite.txt","r")
+	f_in_2 = open( in_path + "all_id_year_title.txt", "r")
+
+	id_year_title = f_in_2.readlines()
+	cite = f_in_1.readlines()
 
 	f_in_1.close()
 	f_in_2.close()
-	item_id = {}
+	item = {}
 
-	for i in t_year:
-		t = i.split(",")
-		t1 = int(t[0])
-		t2 = int(t[1])
-		if (t2 != -1):
-			item_id[t1] = t2
+	for i in id_year_title:
+		parse = i.split("|")
+		item_id = parse[0].strip()
+		year = int(parse[1])
+		if (year not in item):
+			item[item_id] = year
 
-
-	f_error = open("cite_year_divider_error.txt","w")
-	for i in range(1936, 2014):
-		print i
-		f_out = open("./divided_by_year_data2/" +str(i) + "_cite.csv","w")
+	f_out_cited_count_in_the_title_list = open("../2.processed_data/cited_count_in_the_title_list.txt","w")
+	f_out_cited_count_out_the_title_list = open("../2.processed_data/cited_count_out_the_title_list.txt","w")
+	for i in range(1951, 2016):
+		f_out = open(out_path+str(i) + "_cite.csv","w")
 		f_out.write("Source,Target\n")
-		for j in t_cite:
-			tt = j.split(",")
-			id1 = int(tt[0])
-			id2 = int(tt[1])
-			try:
-				if (item_id[id1] <= i and item_id[id2] <=i):
-					f_out.write(str(id1) + ","+ str(id2) + "\n")
-			except:
-				f_error.write(str(id1) + "," +str(id2)+"\n")
+		for j in cite:
+			parse = j.split(",")
+			source = parse[0].strip()
+			target = parse[1].strip()
+			
+			if (target not in item):
+				
+			else:
+
+			
+		print len(cite)
+		print error_cnt
+		print error_cnt2
+		error_cnt = 0
+		error_cnt2= 0
 		f_out.close()
+	f_error.close()
