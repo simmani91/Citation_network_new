6e5ea42e2cf6745f164f5e7678e5e1c51fac93b5
simmani91 <devsimmani@gmail.com>
2016-04-27 09:40:19 +0900

    중심성을 계산하였는데 생각보다 시간이 많이 걸린다. Katz와 Eigenvector는 수렴하지는 않다고 한다. 1956년부터, 네트워크에 뭔가 문제가 있나?

--- code.py
+++ code.py
@@ -25,112 +25,154 @@ def templets():
 
 #네트워크 관련 자료부터 시작한다. 
 
-def data_descriptor(in_path, out_path):
-
-	#입력받는 부분
+def cite_year_divider(in_path, out_path):
+	#연도별로 인용 링크를 나누어준다.
+	#363초 걸림 
+	start_time = time.time()
+	temp_start_time = time.time()
+	print "=============	cite_year_divider start		=============="
+	print "=============		from 1951 to 2015		=============="
 	f_in_1 = open(in_path + "all_cite.txt","r")
 	f_in_2 = open(in_path + "all_id_year_title.txt", "r")
 
-	f_out_source = open(out_path + "source_count.txt", "w")
-	f_out_target = open(out_path + "target_count.txt", "w")
-
-	id_year_title = f_in_2.readlines()
-	cite = f_in_1.readlines()
-	f_in_1.close()
-	f_in_2.close()
-
-	print "all item size:\t" + str(len(id_year_title))
-
-
-	#계산하는 부분 
-	item = {}
-	all_item = {}
-	for i in id_year_title:
-		parse = i.split("|")
-		item_id = parse[0]
-		year = int(parse[1])
-		if (year not in item):
-			item[item_id] = year
-
-			source_count = {}
-			target_count = {}
-
-			print "all relation size:\t" + str(len(cite))
-			for i in cite:
-				parse = i.split(",")
-				source = parse[0].strip()
-				target = parse[1].strip()
-				if (source not in source_count):
-					source_count[source] = 0
-					if (target not in target_count):
-						target_count[target] = 0
-
-						source_count[source] = source_count[source] + 1
-						target_count[target] = target_count[target] + 1
-
-						print "all_source:\t" + str(len(source_count))
-						print "all_target:\t" + str(len(target_count))
-
-	#출력하는 부분
-
-	for i in source_count:
-		f_out_source.write(str(source_count[i]) + "\n")
-		if i < 10000:
-			continue
-			for i in target_count:
-				if i < 10000:
-					continue
-					f_out_target.write(str(target_count[i]) + "\n")
-
-					def plt_hist(x, title):
-						import numpy as np
-						import matplotlib
-						import matplotlib.pyplot as plt
-
-						matplotlib.use('Agg')
-
-						plt.hist(x, 5)
-
-						plt.xlabel('cited_count')
-						plt.ylabel('number_of_data')
-						plt.title('Hist_of_'+ title)
-						plt.axis([0, 200,10000,1050000])
-
-						plt.grid(True)
-						plt.show()
-
-						def data_descriptor_2(in_path, out_path):
-							import matplotlib.pyplot as plt
-
-def cite_data_refining(in_path, out_path):
-	f_in_1 = open(in_path + "all_cite.txt","r")
-	f_in_2 = open( in_path + "all_id_year_title.txt", "r")
-
-	id_year_title = f_in_2.readlines()
-	cite = f_in_1.readlines()
+	t_year = f_in_2.readlines()
+	t_cite = f_in_1.readlines()
 
 	f_in_1.close()
 	f_in_2.close()
-	item = {}
+	item_id = {}
 
-	for i in id_year_title:
-		parse = i.split("|")
-		item_id = parse[0].strip()
-		year = int(parse[1])
-		if (year not in item):
-			item[item_id] = year
+	for i in t_year:
+		t = i.split("|")
+		item = t[0].strip()
+		year = int(t[1])
+		item_id[item] = year
 
-	f_out_cited_count_in_the_title_list = open("../2.processed_data/cited_count_in_the_title_list.txt","w")
-	f_out_cited_count_out_the_title_list = open("../2.processed_data/cited_count_out_the_title_list.txt","w")
+	f_error = open("cite_year_divider_error.txt","w")
+	for i in range(1951, 2016):
+		print i
+		f_out = open(out_path +str(i) + "_cite.csv","w")
+		f_out.write("Source,Target\n")
+		for j in t_cite:
+			tt = j.split(",")
+			source = tt[0].strip()
+			target = tt[1].strip()
+			if (source in item_id and target in item_id):
+				if (item_id[source] <= i and item_id[target] <=i):
+					f_out.write(str(source) + ","+ str(target) + "\n")
+			else:
+				print "error"
+				f_error.write(str(target) + "," +str(target)+"\n")
+		f_out.close()
 
-	for j in cite:
-		parse = j.split(",")
-		source = parse[0].strip()
-		target = parse[1].strip()
+	print "=============	cite_year_divider end	=============="
+	print("takes %s seconds" % (time.time() - temp_start_time))
+	temp_start_time = time.time()
 
-		if (target not in item):
-			f_out_cited_count_out_the_title_list.write(j)
-		else:
-			f_out_cited_count_in_the_title_list.write(j)
-	f_out_cited_count_in_the_title_list.close()
-	f_out_cited_count_out_the_title_list.close()
+def make_net_initialize(in_path):
+	#연도별 중심성을 저장하는 Dump를 초기화해주는 코드라 한다. 
+	global Dump
+	f_in = open(in_path + "2015_cite.csv", "r")
+	lines = f_in.readlines()
+	
+	for line in lines:
+		data = line.split(",")
+		Dump[data[0].strip()] = {}
+		Dump[data[1].strip()] = {}
+
+def make_net(centrality_name, in_path, out_path):
+	#네트워크를 만들고 Centurality를 계산하고 저장할 것이다.
+	import networkx as nx
+	global Dump
+	Dump = {}
+	make_net_initialize(in_path)
+	start_time = time.time()
+	temp_start_time = time.time()
+
+	print "=============		make_net start:" + centrality_name + "		=============="
+	print "=============		from 1951 to 2015		=============="
+
+	for year in range(1951, 2016):
+		print year
+		f_in = open(in_path + str(year) + "_cite.csv","r")
+		lines = f_in.readlines()
+		f_in.close()
+		edge_list = []
+
+		for line in lines:
+			data = line.split(",")
+			data_tuple = (data[0].strip(), data[1].strip())
+			edge_list.append(data_tuple)
+
+		Net = nx.DiGraph(edge_list)
+		Cen_in = {}
+		if (centrality_name == "in_degree"):
+			Cen_in = nx.in_degree_centrality(Net)
+		elif (centrality_name == "degree"):
+			Cen_in = nx.degree_centrality(Net)
+		elif (centrality_name == "eigenvector"):
+			Cen_in = nx.eigenvector_centrality_numpy(Net)
+		elif (centrality_name == "katz"):
+			Cen_in = nx.katz_centrality(Net)
+		elif (centrality_name == "pagerank"):
+			Cen_in = nx.pagerank(Net)
+		elif (centrality_name == "communicability"):
+			Net = nx.Graph(edge_list)
+			Cen_in = nx.communicability_centrality(Net)
+		elif (centrality_name == "load"):
+			Cen_in = nx.load_centrality(Net)
+		
+		for j in Cen_in:
+			key = j
+			val = Cen_in[j]
+			Dump[key][year] = val
+
+	#저장하는 코드 
+	f_out = open(out_path + centrality_name +"_centrality.csv", "w")
+	for key in Dump:
+		line = str(key)
+		for year in range(1951, 2016):
+			data = Dump[key].get(year, 0)
+			line = line + ","+ str(data)
+		line = line + "\n"
+		f_out.write(line)
+	f_out.close()
+
+	print "=============		make_net end			=============="
+	print(centrality_name + "takes %s seconds" % (time.time() - temp_start_time))
+	temp_start_time = time.time()
+
+
+def make_cited_count_sheet(in_path, out_path):
+	start_time = time.time()
+	temp_start_time = time.time()
+
+	print "=============		make_cited_count_sheet		=============="
+	print "=============		from 1951 to 2015		=============="
+
+	global Dump
+	Dump = {}
+
+	make_net_initialize(in_path)
+
+	for year in range(1951, 2016):
+		print year
+		f_in = open( in_path + str(year) + "_cite.csv", "r")
+		lines = f_in.readlines()
+		edge_list = []
+		for line in lines:
+			data = line.split(",")
+			key = data[1].strip()
+			Dump[key][year] = Dump[key].get(year,0) + 1
+
+	f_out = open(out_path + "cited_count.csv", "w")
+	for key in Dump:
+		line = str(key)
+		for year in range(1951, 2016):
+			data = Dump[key].get(year, 0)
+			line = line + ","+ str(data)
+		line = line + "\n"
+		f_out.write(line)
+	print "=============		make_cited_count_sheet		=============="
+	print(centrality_name + "takes %s seconds" % (time.time() - temp_start_time))
+	temp_start_time = time.time()
--- execution.py
+++ execution.py
@@ -12,13 +12,28 @@ import _1_raw_to_processed_parser
 #_1_raw_to_processed_parser.check_year_error()
 #_1_raw_to_processed_parser.refining_data()
 
+#data_descriptor_in_path = "../2.processed_data/"
+#data_descriptor_out_path = "../1.raw_data/0.data_description/"
+#code.data_descriptor(data_descriptor_in_path, data_descriptor_out_path)
+#code.cite_data_refining(data_descriptor_in_path, data_descriptor_in_path)
 
-cite_year_divider_in_path = "../2.processed_data/"
-cite_year_divider_out_path = "../3.time_based_data/1.cite_relation_devide"
+#cite_year_divider_in_path = "../2.processed_data/"
+#cite_year_divider_out_path = "../3.time_based_data/1.cite_relation_devide/"
 #code.cite_year_divider(cite_year_divider_in_path, cite_year_divider_out_path)
 
-data_descriptor_in_path = "../2.processed_data/"
-data_descriptor_out_path = "../1.raw_data/0.data_description/"
-#code.data_descriptor(data_descriptor_in_path, data_descriptor_out_path)
-#code.data_descriptor_2(data_descriptor_out_path, data_descriptor_out_path)
-code.cite_data_refining(data_descriptor_in_path, data_descriptor_in_path)
+#중심성을 만들어 계산한다. 
+make_net_in_path = "../3.time_based_data/1.cite_relation_devide/"
+make_net_out_path = "../3.time_based_data/2.centrality_data/"
+
+#code.make_net( "in_degree", make_net_in_path, make_net_out_path)
+#code.make_net( "degree", make_net_in_path, make_net_out_path)
+#실패 수렴불가 1956년부터 code.make_net( "eigenvector", make_net_in_path, make_net_out_path)
+#실패 수렴불가 1956년부터 code.make_net( "katz", make_net_in_path, make_net_out_path)
+#code.make_net( "pagerank", make_net_in_path, make_net_out_path)
+#code.make_net( "communicability", make_net_in_path, make_net_out_path)
+#code.make_net( "load", make_net_in_path, make_net_out_path)
+
+make_cited_count_in_path = "../3.time_based_data/1.cite_relation_devide/"
+make_cited_count_out_path = "../3.time_based_data/2.centrality_data/"
+
+code.make_cited_count_sheet(make_cited_count_in_path,make_cited_count_out_path)
